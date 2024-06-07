47a27cbbd17b55369d1ad6785d0a65751cbe14d3
asterparedes <asterparedes0912@gmail.com>
2024-06-06 19:51:12 -0700

    rock paper scissors project

---
+++ rock-paper-scissors/main.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = prompt('Choose: Rock, Paper, or Scissors');
+}
