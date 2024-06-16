f8bb3bfd970e24413f86d00719ba295759d14f24
aster <asterparedes0912@gmail.com>
2024-06-16 16:03:48 -0700

    updated the game

--- rock-paper-scissors/main.js
+++ rock-paper-scissors/main.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -7,5 +10,40 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let choice = prompt('Choose: Rock, Paper, or Scissors');
+    let humanChoice = prompt('Choose: Rock, Paper, or Scissors');
+    return humanChoice
 }
+
+console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    const combinations = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+    };
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (combinations[humanChoice] === computerChoice) {
+        // humanScore++;
+        console.log(`You lose! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`);
+    } else {
+        // computerScore++;
+        console.log(`You win! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`);
+    }
+
+    console.log(`Scores - Human: ${humanScore}, Computer: ${computerScore}`);
+}
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
